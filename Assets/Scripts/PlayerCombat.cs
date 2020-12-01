b46ed25e83ce73d2454deb1ab0c0fa013d7a5c89
Michael Nickels <mcnickels55@gmail.com>
2020-12-01 17:56:29 -0500

    GrassAndSceneTest
    GrassAndSceneTest

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public int playerHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHp = PlayerPrefs.GetInt("hp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
